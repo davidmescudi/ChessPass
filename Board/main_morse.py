9dd63c327ad37ab28bae95a725b0dbe69b2c7b40
Paul Seipl <paulseipl@gmail.com>
2024-10-03 20:50:13 +0200

    remove stuff, add prints

--- Board/main_morse.py
+++ Board/main_morse.py
@@ -1,4 +1,4 @@
-from machine import Pin, Timer
+from machine import Pin
 from time import ticks_ms
 from lib import HallSensor, MorseReceiver, DISPLAY_FRAMEBUF, Button
 from lib.config import (
@@ -16,46 +16,62 @@ from lib.config import (
     FIGURE_DETECTION_THRESHOLD,
 )
 
+print("start")
+
+
 def shift_hex_string(s, shift, decrypt=False):
     if decrypt:
         shift = (-1) * shift
     shifted = ""
     for char in s:
         if char.isdigit():  # Shift digits 0-9
-            shifted += chr((ord(char) - ord('0') + shift) % 10 + ord('0'))
+            shifted += chr((ord(char) - ord("0") + shift) % 10 + ord("0"))
         elif char.isalpha():  # Shift letters a-f or A-F (for hex)
             if char.islower():  # Handle lowercase hex letters
-                start = ord('a')
+                start = ord("a")
                 shifted += chr((ord(char) - start + shift) % 16 + start)
             elif char.isupper():  # Handle uppercase hex letters
-                start = ord('A')
+                start = ord("A")
                 shifted += chr((ord(char) - start + shift) % 16 + start)
         else:
             # Leave any other characters unchanged
             shifted += char
     return shifted
 
-display = DISPLAY_FRAMEBUF(DISPLAY_PINS['SPI'], DISPLAY_PINS['CS'], DISPLAY_PINS['DC'], DISPLAY_PINS['BL'] , DISPLAY_PINS['RST'])
+
+print("before display")
 # Global variables
+display = DISPLAY_FRAMEBUF(
+    DISPLAY_PINS["SPI"],
+    DISPLAY_PINS["CS"],
+    DISPLAY_PINS["DC"],
+    DISPLAY_PINS["BL"],
+    DISPLAY_PINS["RST"],
+)
 required_shares = 2
-display = DISPLAY_FRAMEBUF(DISPLAY_PINS['SPI'], DISPLAY_PINS['CS'], DISPLAY_PINS['DC'], DISPLAY_PINS['BL'], DISPLAY_PINS['RST'])
 is_secret_shown = False
 is_receiving_piece_config_mode = True
-right_btn = Pin(3, Pin.IN, Pin.PULL_UP)
+
+print("before buttons")
+
 
 def left_button():
     print("Left Button pressed")
     display.showText("Left Button")
 
+
 def right_button():
     print("Right Button pressedt")
     display.showText("Right Button")
 
+
 left_btn = Button(pin_num=1, callback=left_button)
 right_btn = Button(pin_num=3, callback=right_button)
 
+print("display showtext")
 # Display initial state
-display.showText("Receive Morse")
+display.showText("Morse")
+print("after showtext")
 
 
 def handle_display(activeFigures, messages):
@@ -72,15 +88,18 @@ def handle_display(activeFigures, messages):
         if is_secret_shown:
             return
         display.showSecret(shifted_messages)
-        
-def get_magnet_configs(hall_sensor):
-    pass
+
+
+# def get_magnet_configs(hall_sensor):
+#     pass
+print("start main loop")
+
 
 def main_loop():
     global is_receiving_piece_config_mode, switch_mode_flag
     hall_sensors = []
     morse_receivers = []
-    
+
     # Initialize hall sensors and Morse receivers
     for pin_num in ADCS:
         hall_sensor = HallSensor(
@@ -88,7 +107,7 @@ def main_loop():
             hall_variance_threshold=HALL_VARIANCE_THRESHOLD,
             verbose=True,
             figure_detection_time=FIGURE_DETECTION_TIME,
-            figure_detection_threshold=FIGURE_DETECTION_THRESHOLD
+            figure_detection_threshold=FIGURE_DETECTION_THRESHOLD,
         )
         hall_sensors.append(hall_sensor)
         morse_receivers.append(
@@ -108,15 +127,14 @@ def main_loop():
     # Initialize Morse receivers
     for morse_receiver in morse_receivers:
         morse_receiver.init()
-    
+
     messages = set()
 
     while True:
         active_figures = 0
-        
 
         if is_receiving_piece_config_mode:
-            pass
+            print("in here")
         else:
             # Process each morse receiver
             for index, morse_receiver in enumerate(morse_receivers):
@@ -125,9 +143,10 @@ def main_loop():
                     message = message.lower()
                     messages.add((index, message))
                     print(messages)  # Debugging output
-            
+
             # Update the display
             handle_display(active_figures, messages)
+
 
 # Uncomment to run the main loop
 main_loop()
